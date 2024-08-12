29683a5472196672f471a18bb7dd3451858aab5d
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2024-08-12 10:49:29 +0200

    Webdav datastore

--- playbooks/templates/docker/lawa/start.sh
+++ playbooks/templates/docker/lawa/start.sh
@@ -12,6 +12,9 @@ docker compose up -d app
 echo -e "Create extension Postgis..."
 docker compose exec postgres bash -c 'psql -v ON_ERROR_STOP=1 -U postgres < /var/lib/postgresql/scripts/10-lawa.sql'
 
+echo -e "Retrieve .gpx files from webdav store..."
+docker compose exec postgres bash -c '/var/lib/postgresql/scripts/sync_data_store.sh'
+
 echo -e "Import .gpx tracks into table..."
 docker compose exec postgres bash -c '/var/lib/postgresql/scripts/20-lawa.sh'
 
