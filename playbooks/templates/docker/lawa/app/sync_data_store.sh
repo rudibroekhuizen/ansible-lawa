b9fbe363e5c31d1fe8017c059dc8d704ae2b7209
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2024-12-28 19:33:19 +0100

    Add script

--- playbooks/templates/docker/lawa/app/sync_data_store.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-echo -e "Sync images from remote webdav store..."
-dav -u $WEBDAV_USER -p $WEBDAV_PASSWORD sync --delete dav://lawa/images assets/images
--- playbooks/templates/docker/lawa/postgres/scripts/30-lawa.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-
-psql -v ON_ERROR_STOP=1 < /var/lib/postgresql/scripts/30-lawa.sql
